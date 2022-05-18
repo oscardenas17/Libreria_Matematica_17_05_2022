74a5ceaee21cb2b782c5fe2b6e203a61f1c86f11
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-17 21:41:26 -0500

    Redondear el numero si el decimal es mayor a 0

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+//Redondear el numero si el decimal es mayor a 0
+let num1 = 5.6;
+console.log(Math.ceil(num1));
+console.log(Math.round(num1));
+
 //Validar negativo y positivo en dicimal
-console.log(Math.floor(-5.4));
-console.log(Math.floor(-5.5));
-console.log(Math.floor(-5.6));
-console.log(Math.round(-5.4));
-console.log(Math.round(-5.5));
-console.log(Math.round(-5.6));
-console.log("----------");
-console.log(Math.floor(5.4));
-console.log(Math.floor(5.5));
-console.log(Math.floor(5.6));
-console.log(Math.round(5.4));
-console.log(Math.round(5.5));
-console.log(Math.round(5.6));
+// console.log(Math.floor(-5.4));
+// console.log(Math.floor(-5.5));
+// console.log(Math.floor(-5.6));
+// console.log(Math.round(-5.4));
+// console.log(Math.round(-5.5));
+// console.log(Math.round(-5.6));
+// console.log("----------");
+// console.log(Math.floor(5.4));
+// console.log(Math.floor(5.5));
+// console.log(Math.floor(5.6));
+// console.log(Math.round(5.4));
+// console.log(Math.round(5.5));
+// console.log(Math.round(5.6));
 
 
 //Aproximar el numero despues del 5 decimal
