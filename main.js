c59da523f3ea5e4853e26db9c7052a115019ee3e
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-17 21:35:58 -0500

    Mostrar decimales deseados

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // console.log(Math.pow(5,4) )
 
 //Eliminar decimales
-let num = 5.314749;
-console.log(Math.trunc(num));
+//let num = 5.314749;
+//console.log(Math.trunc(num));
+
+//Mostrar decimales deseados
+console.log(num.toFixed(2));
